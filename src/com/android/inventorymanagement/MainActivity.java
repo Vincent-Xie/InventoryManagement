4ee9a9aa4dc91ebbd7eefeda79a8e30970968446
kom551 <kom551@163.com>
2013-04-10 18:41:31 +0800

    1

--- src/com/android/inventorymanagement/MainActivity.java
+++ src/com/android/inventorymanagement/MainActivity.java
@@ -1,11 +1,16 @@
 package com.android.inventorymanagement;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+    public final static String EXTRA_MESSAGE = "com.example.myapp.MESSAGE";
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,4 +22,26 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch(item.getItemId()){
+            case R.id.editProvider:
+                Intent intent = new Intent(this, ProviderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, item.getTitle());
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+    
+    public void privoder(View view){
+        Intent intent = new Intent(this, ProviderActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, "abcde");
+        startActivity(intent);
+    }
+    
 }
